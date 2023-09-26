2826d586cf545f81759150632d0fcfeced95cb00
Russell Montjoy <russellmontjoy@gmail.com>
2023-09-26 10:13:28 -0600

    finished product

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+function refreshPage() {
+  location.reload();
+}
+setInterval(refreshPage, 120000);
+
+$(document).ready(function () {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const formattedDate = month + '-' + day + '-' + year;;
+
+  $("#currentDay").text(formattedDate);
+
+  $(document).on("click", ".save-btn", function () {
+    const timeBlockId = $(this).closest(".time-block").attr("id");
+    const userInput = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockId, userInput);
+  });
+
+  const now = new Date();
+  const currentHour = now.getHours();
+
+  $(".time-block").each(function () {
+    const timeBlockId = $(this).attr("id").replace("hour-", "");
+    const hours = timeBlockId.slice(0, -2);
+    const period = timeBlockId.slice(-2);
+    let hours24Format = parseInt(hours);
+
+    if (period.toUpperCase() === "PM" && hours24Format !== 12) {
+      hours24Format += 12;
+    }
+
+    if (hours24Format < currentHour) {
+      $(this).addClass("past");
+      $(this).find(".description").prop("disabled", true);
+      $(this).find(".save-btn").prop("disabled", true);   
+    } 
+    else if (hours24Format == currentHour) {
+      $(this).addClass("present");
+    } 
+    else {
+      $(this).addClass("future");
+    }
+
+    const userInput = localStorage.getItem("hour-" + timeBlockId);
+    $(this).find(".description").val(userInput);
+
+    if (currentHour == 0 || currentHour < 9) {
+      localStorage.clear();
+    }
+  });
+});
